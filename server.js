e685bde4950481c990d04a720d445b8739175b5e
roin18 <30768797+roin18@users.noreply.github.com>
2017-08-13 12:17:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('this is art 1 ');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'))
 });
 app.get('/article-two', function (req, res) {
   res.send('this is art 2 ');
